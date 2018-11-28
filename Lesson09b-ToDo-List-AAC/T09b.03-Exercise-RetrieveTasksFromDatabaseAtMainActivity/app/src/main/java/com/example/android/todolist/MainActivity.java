b1c302167990183648be1c3db209ed564446e156
JayvyLei Difuntorum <jdifuntorum@jdifuntorum-macbookpro2.roam.corp.google.com>
2018-11-27 19:00:47 -0800

    Lesson 9b exercises up to #9 completed.

--- Lesson09b-ToDo-List-AAC/T09b.03-Exercise-RetrieveTasksFromDatabaseAtMainActivity/app/src/main/java/com/example/android/todolist/MainActivity.java
+++ Lesson09b-ToDo-List-AAC/T09b.03-Exercise-RetrieveTasksFromDatabaseAtMainActivity/app/src/main/java/com/example/android/todolist/MainActivity.java
@@ -18,8 +18,8 @@ package com.example.android.todolist;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
